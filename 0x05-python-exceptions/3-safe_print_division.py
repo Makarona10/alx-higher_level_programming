58a60091d36013a99a7814a14c2924a5f4f15982
Makarona10 <ahmedneyssi@gmail.com>
2024-01-22 18:44:55 +0200

    s

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     r = None
     try:
         r = a / b
-    except (ZeroDivisionError, ValueError):
+    except (ZeroDivisionError):
         pass
     finally:
         if r != None:
