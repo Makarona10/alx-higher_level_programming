d64cc79a81cf62ab11270bddc1b4a9b36fb76e13
Makarona10 <ahmedneyssi@gmail.com>
2024-01-23 14:20:04 +0200

    s

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square doc """
+
+
+class Square:
+    """ This is a square class """
+
+    def __init__(self, size):
+        """
+        Initializes class attributes
+
+        args:
+            size: size of square
+        """
+        self.__size = size
