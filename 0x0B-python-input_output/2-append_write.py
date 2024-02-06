7402d1361024b54396e4da0f3ae4eac9253eadaf
Makarona10 <ahmedneyssi@gmail.com>
2024-02-07 01:19:13 +0200

    s

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,5 @@
 def append_write(filename="", text=""):
     """appending function"""
     with open(filename, "a", encoding="utf-8") as f:
-        f.write(text)
+        a = f.write(text)
+    return a
