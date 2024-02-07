a7d3a08b1e4a92a2231a28f50be8b39d26bdb25b
Makarona10 <ahmedneyssi@gmail.com>
2024-02-07 02:14:58 +0200

    s

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """add item module"""
 from sys import argv
-saveJSON = __import__("5-save_to_json_file").save_to_json_file
-loadJSON = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 fname = 'add_item.json'
 mlist = []
 try:
-    mlist = loadJSON(fname)
+    mlist = load_from_json_file(fname)
 except Exception:
-    saveJSON(mlist, fname)
+    save_to_json_file(mlist, fname)
 
 if len(argv) > 1:
     for i in range(1, len(argv)):
         mlist.append(argv[i])
-        
-    saveJSON(mlist, fname)
+
+    save_to_json_file(mlist, fname)
 
