f2c14787493dae170a20fb2d54d575889e3dc8c5
Makarona10 <ahmedneyssi@gmail.com>
2024-02-09 22:30:44 +0200

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,14 +10,13 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Rectangle initialization"""
-        super().__init__(id)
-        if width is not int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if height is not int:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if x is not int:
+        if type(x) is not int:
             raise TypeError("x must be an integer")
-        if y is not int:
+        if type(y) is not int:
             raise TypeError("y must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
@@ -27,30 +26,43 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         if y < 0:
             raise ValueError("y must be >= 0")
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
-        def area(self):
+    def area(self):
             """Calculates the area of a rectangle"""
             return self.__height * self.__width
         
-        def display(self):
-            """prints in stdout the Rectangle instance"""
-            if self.__width == 0 or self.__height == 0:
+    def display(self):
+        """prints in stdout the Rectangle instance"""
+        if self.__width == 0 or self.__height == 0:
                 print()
-            else:
-                for x in range (self.__y):
-                    print()
-                for i in range (self.__height):
-                    for y in range (self.__x):
-                        print(" ", end="")
-                    for m in range (self.__width):
-                        print("#", end="")
-                    print()
+        else:
+            for x in range (self.__y):
+                print()
+            for i in range (self.__height):
+                for y in range (self.__x):
+                    print(" ", end="")
+                for m in range (self.__width):
+                    print("#", end="")
+                print()
         
-        def __str__(self):
-            """Returns informal representation of a rectangle"""
-            return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+    def __str__(self):
+        """Returns informal representation of a rectangle"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
+    def update(self, *args):
+        if args[0]:
+            self.id = args[0]
+        if args[1]:
+            self.__width = args[1]
+        if args[2]:
+            self.__height = args[2]
+        if args[3]:
+            self.__X = args[3]
+        if args[4]:
+            self.__y = args[4]
+        
