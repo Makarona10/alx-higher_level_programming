4facc3d59bf35803fd14c03a908c3045416614f9
Makarona10 <ahmedneyssi@gmail.com>
2024-02-12 18:44:55 +0200

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,10 +24,10 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -56,6 +56,34 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """x getter method"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """x setter method"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter method"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter method"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__y = value
 
     def area(self):
         """Calculates the area of a rectangle"""
