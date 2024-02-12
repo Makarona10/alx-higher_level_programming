5cbd3e58cabc3a2c1ec3eaeb3cca4a5cd30e959c
Makarona10 <ahmedneyssi@gmail.com>
2024-02-12 19:00:40 +0200

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -53,9 +53,9 @@ class Rectangle(Base):
     def x(self, value):
         """x setter method"""
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("x must be an integer")
         if value <= 0:
-            raise ValueError("height must be > 0")
+            raise ValueError("x must be > 0")
         self.__x = value
 
     @property
@@ -67,9 +67,9 @@ class Rectangle(Base):
     def y(self, value):
         """y setter method"""
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("y must be an integer")
         if value <= 0:
-            raise ValueError("height must be > 0")
+            raise ValueError("y must be > 0")
         self.__y = value
 
     def area(self):
