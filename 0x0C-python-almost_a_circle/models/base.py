0d1b7cbce64a9ab4ed1a392a85911e3be2f55e3f
Makarona10 <ahmedneyssi@gmail.com>
2024-02-13 01:45:56 +0200

    s

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,10 +33,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        else:
-            json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries or [])
 
     @classmethod
     def save_to_file(cls, list_objs):
